1a29826dcbd9644f27a4760ac4333d3a937a322c
Dhiraj <dhiraj.jamia@gmail.com>
2019-05-07 16:28:42 +0530

    Update StringTime.py

--- StringTime.py
+++ StringTime.py
@@ -16,3 +16,23 @@ print("time:", time)
 
 date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 print("date and time:",date_time)	
+
+
+from datetime import datetime
+
+timestamp = 1528797322
+date_time = datetime.fromtimestamp(timestamp)
+
+print("Date time object:", date_time)
+
+d = date_time.strftime("%m/%d/%Y, %H:%M:%S")
+print("Output 2:", d)	
+
+d = date_time.strftime("%d %b, %Y")
+print("Output 3:", d)
+
+d = date_time.strftime("%d %B, %Y")
+print("Output 4:", d)
+
+d = date_time.strftime("%I%p")
+print("Output 5:", d)
