572bf1a3379d56cbfc81fbc54b44e336c4cd96a3
Dhiraj <dhiraj.jamia@gmail.com>
2019-05-10 17:34:38 +0530

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -6,11 +6,24 @@ print(cur1)
  for t in cur1:
     ...:     print(t)
     ...:     
-
+cur1.execute("SELECT COUNT(*) from information_schema.tables where table_schema = 'rastey'")
 cur1.execute("SELECT * FROM information_schema.tables WHERE table_schema ='rastey';")
 
-
+myresult = cur1.fetchall()
+# commit after create, update, and delete tables in database
+mydb.commit()
  for y in cur1:
     ...:     print(y)
+
+
+
+
+
+
+
+
+
+
+
 
 
